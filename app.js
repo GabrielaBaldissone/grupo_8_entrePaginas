929e4e40ab29ecc26984dc660d0608c767f3fe3a
Luis Agustin Torres Olivera <aguchaso155@gmail.com>
2024-06-30 22:06:14 -0300

    correcciones
    no se como anda, pero anda ndea

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+let port =  3030 || process.env.PORT
+
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/index.html'));
+});
+
+app.get("/ProductCart.html", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/ProductCart.html'));
+});
+
+app.get("/login.html", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo con éxito en puerto ${port}`)
+});
