e99f956c94e83ddf53a0354aabfb06b8dac8fb96
Martin Zalazar <martinzetav@gmail.com>
2024-07-29 12:27:10 -0300

    feat: se trabajo en la creacion de los usuarios

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,15 +1,38 @@
-    const datos = {
-        activar: true
-    }
+const fs = require('fs');
+const path = require('path');
+
+const datos = {
+    activar: true
+}
+
+//users.json
+const usersFilePath = path.join(__dirname, '../data/users.json');
+const fileUsers = fs.readFileSync(usersFilePath, 'utf-8');
 
 const usersController = {
     login: (req, res) => {
 
-    res.render("users/login", {'datos': datos});
+        res.render("users/login", {'datos': datos});
     },
     register: (req, res) => {
     
-    res.render("users/register", {'datos': datos});
+        res.render("users/register", {'datos': datos});
+    },
+    createUser: (req, res) =>{
+        const {firstName, lastName, email, phone, password} = req.body;
+        const users = JSON.parse(fileUsers);
+        const newUser = {
+            id: users.length ? users[users.length - 1].id + 1: 1,
+            firstName,
+            lastName,
+            email,
+            phone,
+            password
+        };
+        users.push(newUser);
+        const usersJSON = JSON.stringify(users);
+        fs.writeFileSync(usersFilePath, usersJSON);
+        res.redirect("/users/login");
     }
 };
 
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -7,5 +7,6 @@ router.get('/login', usersController.login);
 
 // RUTA DE REGISTER
 router.get('/register', usersController.register);
+router.post('/register', usersController.createUser);
 
 module.exports = router; 
