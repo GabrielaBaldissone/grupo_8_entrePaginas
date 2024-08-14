e3a0c20addb70ce53719fc60b725190679375cdf
Martin Zalazar <martinzetav@gmail.com>
2024-08-14 11:18:51 -0300

    feat: se implementaron los middlewares de rutas

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const guestMiddleware = require("../middlewares/guestMiddleware");
 const userValidationRules = require("../validators/userValidator");
 const usersController = require("../controllers/usersController.js");
 
 // RUTA DE LOGIN
-router.get('/login', usersController.login);
+router.get('/login', guestMiddleware, usersController.login);
 router.post('/login', usersController.loginProcess);
 
 // RUTA DE REGISTER
-router.get('/register', usersController.register);
+router.get('/register', guestMiddleware, usersController.register);
 router.post('/register', userValidationRules, usersController.createUser);
 
 module.exports = router; 
