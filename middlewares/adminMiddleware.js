bec0f93831a06ddf7d208b91e4d81306d6922776
Martin Zalazar <martinzetav@gmail.com>
2024-08-29 21:35:07 -0300

    se creo middleware para verificar el rol de admin

---
+++ middlewares/adminMiddleware.js
@@ -0,0 +1,17 @@
+function adminMiddleware(req, res, next) {
+    res.locals.isAdmin = false;
+
+    if (req.session.userLogged && req.session.userLogged.rol === "admin") {
+        res.locals.isAdmin = true;
+        next(); // Continúa si es administrador
+    } else {
+        // Solo redirigir si el usuario intenta acceder a la ruta de edición
+        if (req.path.includes('/edit/')) {
+            res.redirect(`/products/detail/${req.params.id}`);
+        } else {
+            next(); // Permitir el acceso a otras rutas sin redirección
+        }
+    }
+}
+
+module.exports = adminMiddleware;
