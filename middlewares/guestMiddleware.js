96e22b2c15e76c1497cfb03f589c590cd47421c5
Martin Zalazar <martinzetav@gmail.com>
2024-08-29 21:43:04 -0300

    se agrego middleware para los invitados

--- middlewares/guestMiddleware.js
+++ middlewares/guestMiddleware.js
@@ -1,7 +1,6 @@
 function guestMiddleware(req, res, next){
     if(req.session.userLogged){
-        // return res.redirect("/users/profile")
-        return res.send("Si estas logeado");
+        return res.redirect("/users/profile")
     }
     next();
 }
