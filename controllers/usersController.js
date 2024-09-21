476b27e68a60f64f56e43316fd23a5298276ee45
Martin Zalazar <martinzetav@gmail.com>
2024-09-21 13:33:18 -0300

    se actualizo el registro y el login del usuario

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,5 @@
 const bcryptjs  = require("bcryptjs");
+const db = require("../database/models");
 const { validationResult } = require("express-validator");
 
 const datos = {
@@ -6,75 +7,105 @@ const datos = {
 }
 
 const usersController = {
-    users: null,
     login: (req, res) => {
         res.render("users/login", {'datos': datos});
     },
     register: (req, res) => {
         res.render("users/register", {'datos': datos});
     },
-    createUser: (req, res) =>{
+    createUser: (req, res) => {
         const errors = validationResult(req);
-        if(errors.isEmpty()){
-            const {firstName, lastName, email, phone, password} = req.body;
-            const userInDB = User.findByField("email", email);
-            if(userInDB){
-                return res.render("users/register", {
-                    errors: {
-                        email: {
-                            msg: "Este email ya está registrado"
-                        }
-                    },
-                    oldData: req.body, datos});
-            }
-            const newUser = {
-                firstName,
-                lastName,
-                email,
-                phone,
-                password: bcryptjs.hashSync(password, 10)
-            };
-            const userCreated = User.create(newUser);
-            return res.redirect("/users/login");
-        }else{
-            return res.render("users/register", {errors: errors.mapped(), oldData: req.body, datos});
+    
+        if (errors.isEmpty()) {
+            const { firstName, lastName, email, phone, password } = req.body;
+    
+            db.User.findOne({ where: { email } })
+                .then(userInDB => {
+                    if (userInDB) {
+                        return res.render("users/register", {
+                            errors: {
+                                email: {
+                                    msg: "Este email ya está registrado"
+                                }
+                            },
+                            oldData: req.body,
+                            datos
+                        });
+                    }
+    
+                    return db.User.create({
+                        first_name: firstName,
+                        last_name: lastName,
+                        email,
+                        phone,
+                        password: bcryptjs.hashSync(password, 10),
+                        rol: "rol"
+                    });
+                })
+                .then(() => {
+                    return res.redirect("/users/login");
+                })
+                .catch(error => {
+                    console.error(error);
+                    return res.render("users/register", {
+                        errors: { general: { msg: "Ocurrió un error al crear el usuario" } },
+                        oldData: req.body,
+                        datos
+                    });
+                });
+        } else {
+            return res.render("users/register", { errors: errors.mapped(), oldData: req.body, datos });
         }
     },
     loginProcess(req, res){
         const { email, password, rememberMe } = req.body;
         
-        const userToLogin = User.findByField("email", email);
-        if(userToLogin){
-            const isOkThePassword = bcryptjs.compareSync(password, userToLogin.password);
-            if(isOkThePassword){
-                delete userToLogin.password;
-                req.session.userLogged = userToLogin;
-                if(rememberMe){
-                    res.cookie("userEmail", email, { maxAge: (1000*60) * 10 });
+        db.User.findOne({where:{email}})
+        .then((userToLogin)=>{
+            if(userToLogin){
+                const isOkThePassword = bcryptjs.compareSync(password, userToLogin.password);
+                if(isOkThePassword){
+                    delete userToLogin.password;
+                    req.session.userLogged = userToLogin;
+                    if(rememberMe){
+                        res.cookie("userEmail", email, { maxAge: (1000*60) * 10 });
+                    }
+                    return res.redirect("/users/profile");
+                }else{
+                    return res.render("users/login", {
+                        errors:{
+                            password: {
+                                msg: "La contraseña que ingresaste es incorrecta"
+                            }
+                        },
+                        oldData: req.body,
+                        datos
+                    })
                 }
-                return res.redirect("/users/profile");
             }else{
                 return res.render("users/login", {
                     errors:{
-                        password: {
-                            msg: "La contraseña que ingresaste es incorrecta"
+                        email: {
+                            msg: "Este email no se encuentra registrado"
                         }
                     },
                     oldData: req.body,
                     datos
                 })
             }
-        }else{
+        })
+        .catch(error => {
+            console.error(error);
             return res.render("users/login", {
-                errors:{
-                    email: {
-                        msg: "Este email no se encuentra registrado"
+                errors: {
+                    general: {
+                        msg: "Ocurrió un error al intentar iniciar sesión, por favor inténtalo nuevamente."
                     }
                 },
                 oldData: req.body,
                 datos
-            })
-        }
+            });
+        });
     },
     getUserProfile(req, res){
         return res.render("users/userProfile", {user: req.session.userLogged, datos});
