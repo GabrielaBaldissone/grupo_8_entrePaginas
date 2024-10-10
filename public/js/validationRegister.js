a2f84666500530bf2edb72c14bacad59b3c8ff89
Katherina Ayelen Juarez <katherinajuarez09@gmail.com>
2024-10-10 12:13:11 -0300

    Update validationRegister.js

--- public/js/validationRegister.js
+++ public/js/validationRegister.js
@@ -13,60 +13,66 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     validation
-    .addField("#firstName", [
-        {
-            rule: "required",
-            errorMessage: 'Nombre requerido',
-        },
-        {
-            rule: "minLength",
-            value: 4,
-            errorMessage: "Como mínimo el nombre debe tener 4 caracteres",
-        }
-    ])
-    .addField("#apellido", [
-        {
-            rule: "required",
-            errorMessage: "Apellido requerido",
-        },
-        {
-            rule: "minLength",
-            value: 4,
-            errorMessage: "Como mínimo el apellido debe tener 4 caracteres",
-        }
-    ])
-    .addField("#email", [
-        {
-            rule: "required",
-            errorMessage: "Email requerido",
-        },
-        {
-            rule: "email",
-            errorMessage: "Email no válido",
-        }
-    ])
-    .addField("#telefono", [
-        {
-            rule: "required",
-            errorMessage: "Teléfono requerido",
-        },
-        {
-            rule: "customRegexp",
-            value: /^\+?\d{10,15}$/,
-            errorMessage: "Teléfono no válido",
-        }
-    ])
-    .addField("#contrasenia", [
-        {
-            rule: "required",
-            errorMessage: "Contraseña requerida", 
-        },
-        {
-            rule: "password",
-            errorMessage: "La contraseña debe contener un mínimo de ocho caracteres, al menos una letra mayúscula y un número", 
-        }
-    ])
-    .onSuccess((event) => {
-        event.target.submit();
-    })
+        .addField("#firstName", [
+            {
+                rule: "required",
+                errorMessage: 'Nombre requerido',
+            },
+            {
+                rule: "minLength",
+                value: 2,
+                errorMessage: "Como mínimo el nombre debe tener 2 caracteres",
+            }
+        ])
+        .addField("#apellido", [
+            {
+                rule: "required",
+                errorMessage: "Apellido requerido",
+            },
+            {
+                rule: "minLength",
+                value: 2,
+                errorMessage: "Como mínimo el apellido debe tener 2 caracteres",
+            }
+        ])
+        .addField("#email", [
+            {
+                rule: "required",
+                errorMessage: "Email requerido",
+            },
+            {
+                rule: "email",
+                errorMessage: "Email no válido",
+            }
+        ])
+        .addField("#telefono", [
+            {
+                rule: "required",
+                errorMessage: "Teléfono requerido",
+            },
+            {
+                rule: "customRegexp",
+                value: /^\+?\d{10,15}$/,
+                errorMessage: "Teléfono no válido",
+            }
+        ])
+        .addField("#contrasenia", [
+            {
+                rule: "required",
+                errorMessage: "Contraseña requerida", 
+            },
+            {
+                rule: "minLength",
+                value: 8,
+                errorMessage: "La contraseña debe tener al menos 8 caracteres",
+            },
+            {
+                rule: "customRegexp",
+                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/,
+                errorMessage: "La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial", 
+            }
+        ])
+        .onSuccess((event) => {
+            event.target.submit();
+        });
 });
