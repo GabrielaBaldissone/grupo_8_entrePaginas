d74204ac7251f0a352ca3412d4fdb2a748b44918
Martin Zalazar <martinzetav@gmail.com>
2024-10-11 15:55:23 -0300

    se completo la validacion para crear producto

--- validators/productValidators.js
+++ validators/productValidators.js
@@ -2,24 +2,38 @@ const { body } = require('express-validator');
 
 const productValidators = [
     body('name')
-        .notEmpty().withMessage('El nombre es obligatorio')
+        .trim().notEmpty().withMessage('El nombre es obligatorio').bail()
         .isLength({ min: 5 }).withMessage('El nombre debe tener al menos 5 caracteres'),
     
-    body('description')
-        .notEmpty().withMessage('La descripción es obligatoria')
-        .isLength({ min: 20 }).withMessage('La descripción debe tener al menos 20 caracteres'),
+    body('stock')
+        .notEmpty().withMessage("El stock es obligatorio").bail()
+        .isInt({min: 0}).withMessage("El stock debe ser un número entero positivo"),
 
-    body('image')
-        .custom((value, { req }) => {
-            if (!req.file) {
-                return true;
+    body("image").custom((value, { req })=>{
+        let file = req.file;
+        let acceptedExtensions = [".jpg", ".jpeg", ".png"];
+        
+        if(!file){
+            req.file = { filename: "default.png"};
+        }else{
+            let fileExtension = path.extname(file.originalname).toLowerCase();
+            if(!acceptedExtensions.includes(fileExtension)){
+                throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(", ")}`);
             }
-            const validExtensions = ['image/jpeg', 'image/png', 'image/gif'];
-            if (!validExtensions.includes(req.file.mimetype)) {
-                throw new Error('La imagen debe ser un archivo válido (JPG, JPEG, PNG, GIF)');
-            }
-            return true;
-        })
+        }
+
+        return true;
+        
+    }),
+
+    body('price')
+        .notEmpty().withMessage("El precio es obligatorio").bail()
+        .isInt({min: 0}).withMessage("El precio debe ser un número entero positivo"),
+        
+    body('description')
+        .trim().notEmpty().withMessage('La descripción es obligatoria').bail()
+        .isLength({ min: 20 }).withMessage('La descripción debe tener al menos 20 caracteres')
+
 ];
 
 module.exports = productValidators;
