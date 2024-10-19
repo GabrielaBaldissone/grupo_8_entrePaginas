bab7d8a411e83641a21e74746ecf52a97d6948bf
Katherina Ayelen Juarez <katherinajuarez09@gmail.com>
2024-10-19 18:57:05 -0300

    Update structure.sql

--- data/structure.sql
+++ data/structure.sql
@@ -1,53 +1,138 @@
-DROP DATABASE IF EXISTS Essenciana;
-CREATE DATABASE Essenciana;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 19-10-2024 a las 21:39:52
+-- Versión del servidor: 8.0.31
+-- Versión de PHP: 8.0.26
 
-USE Essenciana;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- Tabla usuarios
-CREATE TABLE users (
-    id_user INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    phone VARCHAR(50),
-    password VARCHAR(255) NOT NULL,
-    rol ENUM('admin', 'cliente') NOT NULL DEFAULT 'cliente'
-);
 
--- Tabla categorías
-CREATE TABLE categories (
-    id_category INT AUTO_INCREMENT PRIMARY KEY,
-    category VARCHAR(255) NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Tabla productos
-CREATE TABLE products (
-    id_product INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    stock INT NOT NULL,
-    image LONGBLOB,
-    description TEXT,
-    id_category INT,
-    FOREIGN KEY (id_category) REFERENCES categories(id_category)
-);
+--
+-- Base de datos: `bookit`
+--
+CREATE DATABASE IF NOT EXISTS `bookit` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `bookit`;
 
--- Tabla órdenes
-CREATE TABLE orders (
-    id_order INT AUTO_INCREMENT PRIMARY KEY,
-    id_user INT,
-    available BOOLEAN,
-    FOREIGN KEY (id_user) REFERENCES users(id_user)
-);
+-- --------------------------------------------------------
 
--- Tabla productos en las órdenes
-CREATE TABLE order_product (
-    id_order_product INT AUTO_INCREMENT PRIMARY KEY,
-    id_order INT,
-    id_product INT,
-    quantity INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_order) REFERENCES orders(id_order),
-    FOREIGN KEY (id_product) REFERENCES products(id_product)
-);
+--
+-- Estructura de tabla para la tabla `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+CREATE TABLE IF NOT EXISTS `books` (
+  `id_book` int NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `description` text,
+  `image` varchar(255) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `stock` int DEFAULT NULL,
+  `id_category` int DEFAULT NULL,
+  PRIMARY KEY (`id_book`),
+  KEY `id_category` (`id_category`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id_category` int NOT NULL AUTO_INCREMENT,
+  `category` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_category`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id_order` int NOT NULL AUTO_INCREMENT,
+  `id_user` int DEFAULT NULL,
+  `available` tinyint DEFAULT NULL,
+  PRIMARY KEY (`id_order`),
+  KEY `id_user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `order_book`
+--
+
+DROP TABLE IF EXISTS `order_book`;
+CREATE TABLE IF NOT EXISTS `order_book` (
+  `id_order_book` int NOT NULL AUTO_INCREMENT,
+  `id_order` int DEFAULT NULL,
+  `id_book` int DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_order_book`),
+  KEY `id_order` (`id_order`),
+  KEY `id_book` (`id_book`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` enum('admin','cliente') DEFAULT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `books`
+--
+ALTER TABLE `books`
+  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id_category`);
+
+--
+-- Filtros para la tabla `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Filtros para la tabla `order_book`
+--
+ALTER TABLE `order_book`
+  ADD CONSTRAINT `order_book_ibfk_1` FOREIGN KEY (`id_order`) REFERENCES `orders` (`id_order`),
+  ADD CONSTRAINT `order_book_ibfk_2` FOREIGN KEY (`id_book`) REFERENCES `books` (`id_book`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
