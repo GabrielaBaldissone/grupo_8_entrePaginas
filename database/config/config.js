ee8aec827d4a9bd1ccb7d9b9a185eeed11e47d82
Martin Zalazar <martinzetav@gmail.com>
2024-10-26 12:03:33 -0300

    se cambio la conexion a la bd entre_paginas

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "",
-    "database": "essenciana",
+    "database": "entre_paginas",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
